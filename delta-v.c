39e33b1a4e8f2b16ad1fda1bb433d21c9e9062ba
ii-v <iiv@openmailbox.org>
2016-05-26 21:37:48 +0200

    First Commit

---
+++ delta-v.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+float
+rocket_equation(float total_mass, float dry_mass, float sp_impulse)
+/* Using the Tsiolkovsky rocket equation */
+{
+    float delta_v;
+ 
+    delta_v = sp_impulse * 9.82 * log(total_mass / dry_mass);
+    
+    printf("The ΔV of the vehicle is %0.2f meters/second.\n", delta_v);
+}
+
+int
+main()
+{
+    float total_mass; /* The total mass of the vehicle including fuel. */
+    float dry_mass; /* The mass of the vehicle without fuel. */
+    float sp_impulse; /* The specific impulse of the engine. (Fuel effiency) */
+
+    printf("Write the total mass of the vehicle: ");
+    scanf("%f", &total_mass);
+    if (total_mass  
+    printf("Write the mass of the vehicle without fuel: ");
+    scanf("%f", &dry_mass);
+
+    printf("Write the vehicle's engines specific impulse in seconds: ");
+    scanf("%f", &sp_impulse);
+ 
+    rocket_equation(total_mass, dry_mass, sp_impulse);
+    
+    return 0;
+}
