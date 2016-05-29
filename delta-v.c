3a4a8d8cfbaf9b267cf15201e866dd4763c12e7d
ii-v <iiv@openmailbox.org>
2016-05-29 17:56:27 +0200

    Added multiple stages

--- delta-v.c
+++ delta-v.c
@@ -9,10 +9,8 @@ rocket_equation(float total_mass, float dry_mass, float sp_impulse)
     float g = 9.81; /* Gravitational Constant */
 
     delta_v = sp_impulse * g * log(total_mass / dry_mass);
-    
-    printf("The ΔV of the vehicle is %0.2f meters/second.\n", delta_v);
-    
-    return 0;
+          
+    return delta_v;
 }
 
 int
@@ -21,17 +19,51 @@ main()
     float total_mass; /* The total mass of the vehicle including fuel. */
     float dry_mass; /* The mass of the vehicle without fuel. */
     float sp_impulse; /* The specific impulse of the engine. (Fuel effiency) */
+    int num_stages; /* The amount of stages of the vehicle. */
+    float delta_v;
+    float total_dv = 0;
 
-    printf("Write the total mass of the vehicle: ");
-    scanf("%f", &total_mass);
- 
-    printf("Write the mass of the vehicle without fuel: ");
-    scanf("%f", &dry_mass);
+    printf("How many stages does your vehicle have? ");
+    scanf("%i", &num_stages);
+    
+    if (num_stages == 1)
+    {
+        printf("Write the total mass of the vehicle: ");
+        scanf("%f", &total_mass);
+     
+        printf("Write the mass of the vehicle without fuel: ");
+        scanf("%f", &dry_mass);
 
-    printf("Write the vehicle's engines specific impulse in seconds: ");
-    scanf("%f", &sp_impulse);
- 
-    rocket_equation(total_mass, dry_mass, sp_impulse);
+        printf("Write the vehicle's engines specific impulse in seconds: ");
+        scanf("%f", &sp_impulse);
+     
+        delta_v = rocket_equation(total_mass, dry_mass, sp_impulse);
+        
+        printf("The total Delta-V is %0.2f.\n", delta_v);
+    }
+
+    else if (num_stages > 1)
+    {     
+        while (num_stages > 0)
+        {
+
+            printf("Write the total mass of stage %i: ", num_stages);
+            scanf("%f", &total_mass);
+         
+            printf("Write the mass of stage %i without fuel: ", num_stages);
+            scanf("%f", &dry_mass);
+
+            printf("Write stage %i engine's specific impulse in seconds: ", num_stages);
+            scanf("%f", &sp_impulse);
+         
+            delta_v = rocket_equation(total_mass, dry_mass, sp_impulse);
+            total_dv = total_dv + delta_v;
+            
+            num_stages = num_stages - 1;
+        }
+        
+        printf("The total Delta-V is %0.2f.\n", total_dv);
+    }
     
     return 0;
 }
